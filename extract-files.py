116504da845833e69cacc2a0fba7b96be8f8df7d
Pranav Vashi <neobuddy89@gmail.com>
2025-12-30 20:55:10 +0530

    lexus: Extract only for device path
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -97,7 +97,5 @@ module = ExtractUtilsModule(
 )
 
 if __name__ == '__main__':
-    utils = ExtractUtils.device_with_common(
-        module, 'sm8650-common', module.vendor
-    )
+    utils = ExtractUtils.device(module)
     utils.run()
